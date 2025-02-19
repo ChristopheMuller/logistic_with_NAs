da1f09c8e21e670c28a28c00f58a172f85047f8c
Christophe Muller <mullec@ethz.ch>
2025-02-19 17:46:13 +0100

    typo

--- methods_in_R.R
+++ methods_in_R.R
@@ -1,7 +1,7 @@
 library(mice)
 library(dplyr)
 library(stringr)
-library(misaem_fork)
+library(misaem.fork)
 
 # Base class for imputation methods
 ImputationMethod <- R6::R6Class("ImputationMethod",
