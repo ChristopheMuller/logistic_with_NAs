f8c80ec89d0312b2cbf8cda65be8393247c088a1
mullerchristophe21 <mullerchristophe21@gmail.com>
2025-04-09 16:51:59 +0200

    mice cart procedure

--- methods_in_R.R
+++ methods_in_R.R
@@ -166,6 +166,140 @@ MICELogisticRegression <- R6::R6Class("MICELogisticRegression",
 )
 
 
+MICECartLogisticRegression <- R6::R6Class("MICECartLogisticRegression",
+                                      inherit = ImputationMethod,
+                                      public = list(
+                                        n_imputations = 5,
+                                        maxit = 5,
+                                        mask = FALSE,
+                                        add.y = FALSE,
+                                        
+                                        initialize = function(name, n_imputations = 5, maxit = 5, mask = FALSE, add.y = FALSE) {
+                                          super$initialize(name)
+                                          self$n_imputations <- n_imputations
+                                          self$maxit <- maxit
+                                          self$mask <- mask
+                                          self$add.y <- add.y
+                                        },
+                                        
+                                        fit = function(X_train, M_train, y_train, X_test = NULL, M_test = NULL) {
+                                          # Combine training data for imputation
+                                          data_train <- as.data.frame(X_train)
+                                          
+                                          # Add y to data if add.y is TRUE
+                                          if (self$add.y) {
+                                            data_train$y <- y_train
+                                          }
+                                          
+                                          # Create ignore vector for MICE
+                                          ignore_vec <- rep(FALSE, nrow(data_train))
+                                          
+                                          # If test set is provided
+                                          if (!is.null(X_test)) {
+                                            data_test <- as.data.frame(X_test)
+                                            
+                                            # Add y (as NA) to test data if add.y is TRUE
+                                            if (self$add.y) {
+                                              data_test$y <- NA
+                                            }
+                                            
+                                            # Combine train and test data
+                                            data_full <- rbind(data_train, data_test)
+                                            
+                                            # Update ignore vector
+                                            ignore_vec <- c(ignore_vec, rep(TRUE, nrow(data_test)))
+                                            
+                                            # Run MICE on full dataset
+                                            self$imputation_model <- mice(data_full, m = self$n_imputations, 
+                                                                          maxit = self$maxit, 
+                                                                          printFlag = FALSE, 
+                                                                          ignore = ignore_vec, method="cart")
+                                          } else {
+                                            # Run MICE only on training data
+                                            self$imputation_model <- mice(data_train, m = self$n_imputations, 
+                                                                          maxit = self$maxit, 
+                                                                          printFlag = FALSE, method="cart")
+                                          }
+                                          
+                                          # Fit logistic regression on each imputed training dataset
+                                          models <- list()
+                                          for(i in 1:self$n_imputations) {
+                                            # Complete only the training data
+                                            imp_train_data <- complete(self$imputation_model, i)[!ignore_vec, ]
+                                            
+                                            # Remove y if it was added during imputation
+                                            if (self$add.y) {
+                                              imp_train_data <- imp_train_data[, !names(imp_train_data) %in% "y"]
+                                            }
+                                            
+                                            # Add mask before logistic regression if mask is TRUE
+                                            if (self$mask) {
+                                              imp_train_data <- cbind(imp_train_data, as.data.frame(M_train))
+                                            }
+                                            
+                                            
+                                            # Fit logistic regression
+                                            formula <- as.formula(paste("y ~", paste(names(imp_train_data)[names(imp_train_data) != "y"], 
+                                                                                     collapse = " + ")))
+                                            models[[i]] <- glm(formula, family = binomial(), data = cbind(imp_train_data, y = y_train))
+                                          }
+                                          
+                                          # Store models
+                                          self$model <- models
+                                          TRUE
+                                        },
+                                        
+                                        predict_probs = function(X_new, M_new) {
+                                          # Predict for each imputed model
+                                          pred_probs <- matrix(0, nrow = nrow(X_new), ncol = self$n_imputations)
+                                          
+                                          for(i in 1:self$n_imputations) {
+                                            # Complete the test data using the same imputation model
+                                            imp_test <- complete(self$imputation_model, i)[nrow(self$imputation_model$data) - nrow(X_new) + 1:nrow(X_new), ]
+                                            
+                                            # Remove y if it was added during imputation
+                                            if (self$add.y) {
+                                              imp_test <- imp_test[, !names(imp_test) %in% "y"]
+                                            }
+                                            
+                                            # Add mask before prediction if mask is TRUE
+                                            if (self$mask) {
+                                              imp_test <- cbind(imp_test, as.data.frame(M_new))
+                                            }
+                                            
+                                            # Predict probabilities
+                                            pred_probs[,i] <- predict(self$model[[i]], newdata = imp_test, type = "response")
+                                          }
+                                          
+                                          # Average predictions across imputations
+                                          return(rowMeans(pred_probs))
+                                        },
+                                        
+                                        return_params = function() {
+                                          if (!self$return_beta) return(NULL)
+                                          
+                                          # Pool coefficients using Rubin's rules
+                                          coef_list <- lapply(self$model, coef)
+                                          pooled_coef <- Reduce('+', coef_list) / length(coef_list)
+                                          
+                                          # Separate intercept and coefficients
+                                          intercept <- pooled_coef[1]  # First coefficient is intercept in R
+                                          coefficients <- pooled_coef[-1]  # All other coefficients
+                                          
+                                          # Remove names from the vectors
+                                          names(intercept) <- NULL
+                                          names(coefficients) <- NULL
+                                          
+                                          # Create the exact string format to match Python output
+                                          coef_str <- paste(coefficients, collapse = ", ")
+                                          int_str <- as.character(intercept)
+                                          
+                                          return(sprintf("[[%s], [%s]]", coef_str, int_str))
+                                        }
+                                      )                     
+)
+
+
 SAEMLogisticRegression <- R6::R6Class("SAEMLogisticRegression",
                                       inherit = ImputationMethod,
                                       public = list(
@@ -192,7 +326,7 @@ SAEMLogisticRegression <- R6::R6Class("SAEMLogisticRegression",
                                           colnames(X_test) <- paste0("X", 1:ncol(X_new))
                                           
                                           # Get predictions
-                                          pred_probs <- predict(self$model, newdata = X_test, type = "response")
+                                          pred_probs <- predict(self$model, newdata = X_test, type = "response", mcmc_map=500)
 
                                           return(pred_probs)
                                         },
