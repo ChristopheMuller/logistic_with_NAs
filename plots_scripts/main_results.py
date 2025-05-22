5d293e58a2aafc9b915db129b52e8b3d3390f7a7
mullerchristophe21 <mullerchristophe21@gmail.com>
2025-05-22 14:47:18 +0200

    plot general results

---
+++ plots_scripts/main_results.py
@@ -0,0 +1,84 @@
+#####
+#
+# 
+#
+#####
+
+# %% load packages
+
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from setups import metrics_config, methods_config
+
+# %% set up
+
+exp = "ExpA"
+score_matrix = pd.read_csv(os.path.join("..", "data", exp, "score_matrix.csv"))
+
+
+metrics_config
+
+# %% 1st Plot : columns of each score
+
+methods_sel = ['SAEM', 'CC', '05.IMP', 'Mean.IMP', 'Mean.IMP.M', 'PbP',
+     'MICE.Y.IMP', 'MICE.10.Y.IMP', 'MICE.100.Y.IMP']
+
+scores_sel = ["misclassification", "calibration", "mse_error", "mae_bayes"]
+filter_bayes = [True, True, False, False]
+
+ntrains = [500, 1000, 5000, 10000, 50000]
+
+ylims = [(-0.001,0.04), (-0.001,0.03), (-0.01, 0.45), (-0.005, 0.17)]
+
+# one sub-plot for each score
+
+fig, axes = plt.subplots(1, len(scores_sel), figsize=(4 * len(scores_sel), 5))
+
+for i, score in enumerate(scores_sel):
+
+    print(i, score)
+
+    # filter the score
+    score_matrix_sel = score_matrix[score_matrix["metric"] == score]
+    score_matrix_sel = score_matrix_sel[score_matrix_sel["method"].isin(methods_sel)]
+    score_matrix_sel = score_matrix_sel[score_matrix_sel["exp"] == exp]
+    score_matrix_sel = score_matrix_sel[score_matrix_sel["n_train"].isin(ntrains)]
+    score_matrix_sel = score_matrix_sel[score_matrix_sel["bayes_adj"] == filter_bayes[i]]
+
+    # group by method and n_train
+    score_matrix_sel = score_matrix_sel.groupby(["method", "n_train"]).agg({"score": ["mean", "std", "count"]}).reset_index()
+    score_matrix_sel.columns = ["method", "n_train", "mean", "sd", "count"]
+    score_matrix_sel["se"] = score_matrix_sel["sd"] / np.sqrt(score_matrix_sel["count"])
+
+    # plot the mean and se
+    for method in methods_sel:
+
+        method_config = methods_config[method]
+
+        score_matrix_method = score_matrix_sel[score_matrix_sel["method"] == method]
+        axes[i].plot(score_matrix_method["n_train"], score_matrix_method["mean"], label=method, 
+                     color=method_config["color"], linestyle=method_config["linestyle"],
+                     marker=method_config["marker"], markersize=5)
+        axes[i].fill_between(score_matrix_method["n_train"], score_matrix_method["mean"] - score_matrix_method["se"],
+                              score_matrix_method["mean"] + score_matrix_method["se"], alpha=0.2, 
+                              color=method_config["color"], linestyle=method_config["linestyle"])
+    
+    axes[i].set_xscale("log")
+    axes[i].set_xlabel("Number of training samples")
+    axes[i].set_ylabel(metrics_config[score]["label"])
+    axes[i].set_title(metrics_config[score]["label"])   
+    if i == 0:
+        axes[i].legend()
+    # axes[i].grid()
+    axes[i].set_ylim(ylims[i])
+
+    # line at 
+    axes[i].axhline(0, color="black", linestyle="--", linewidth=0.5)
+
+plt.tight_layout()
+plt.savefig(os.path.join("..", "plots_scripts", "plots", "expA_general.pdf"))
+plt.show()
+    
---
+++ plots_scripts/setups.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+color_palette = plt.cm.tab10.colors
+
+uncertainties = {
+    "sd": np.std,
+    "se": lambda x: np.std(x) / np.sqrt(len(x)),
+}
+
+uncertainties_config = {
+    "sd": {"label": "St. Dev."},
+    "se": {"label": "St. Err."},
+}
+
+variable_config = {
+    "corr": {"label": "Correlation"},
+    "prcNA": {"label": "Missingness"},
+    "prop1": {"label": "Proportion of 1"},
+    "d": {"label": "Dimensions"},
+    "centricity": {"label": "Centricity"},
+}
+
+metrics_config = {
+    "angular_error": {"label": "Angular Error"},
+    "mse_error": {"label": "MSE"},
+    "misclassification": {"label": "Misclassification"},
+    "brier_score": {"label": "Brier Score"},
+    "mae_score": {"label": "MAE"},
+    "running_time": {"label": "Running Time (s.)"},
+    "mae_bayes": {"label": "MAE to Bayes"},
+    "calibration": {"label": "Calibration"}
+}
+
+methods_config = {
+    "05.IMP": {"label": "05.IMP", "color": color_palette[0], "linestyle": "-", "marker":"o"},
+    "05.IMP.M": {"label": "05.IMP.M", "color": color_palette[0], "linestyle": "--", "marker":"x"},
+
+    "CC": {"label": "CC", "color": color_palette[1], "linestyle": "-", "marker":"o"},
+
+    "PbP": {"label": "PbP", "color": color_palette[2], "linestyle": "-", "marker":"o"},
+
+    "Mean.IMP": {"label": "Mean.IMP", "color": color_palette[3], "linestyle": "-", "marker":"o"},
+    "Mean.IMP.M": {"label": "Mean.IMP.M", "color": color_palette[3], "linestyle": "--", "marker":"x"},
+
+    "MICE.Y.IMP": {"label": "MICE.1.Y.IMP", "color": color_palette[4], "linestyle": "-", "marker":"o"},
+    "MICE.Y.M.IMP": {"label": "MICE.1.Y.IMP.M", "color": color_palette[4], "linestyle": "--", "marker":"x"},
+
+    "SAEM": {"label": "SAEM", "color": color_palette[5], "linestyle": "-", "marker":"o"},
+
+    "MICE.10.Y.IMP": {"label": "MICE.10.Y.IMP", "color": color_palette[6], "linestyle": "-", "marker":"o"},
+    "MICE.100.Y.IMP": {"label": "MICE.100.Y.IMP", "color": color_palette[6], "linestyle": "--", "marker":"x"},
+
+    "MICE.IMP": {"label": "MICE.1.IMP", "color": color_palette[7], "linestyle": "-", "marker":"o"},
+    "MICE.M.IMP": {"label": "MICE.1.IMP.M", "color": color_palette[7], "linestyle": "--", "marker":"x"},
+
+
+
+#     "MICE.5.IMP": {"label": "MICE.5.IMP", "color": color_palette[2], "linestyle": "--", "marker":"x"},
+    "PY.SAEM": {"label": "PY.SAEM", "color": color_palette[5], "linestyle": "--", "marker":"x"},
+#     "SAEM.MI": {"label": "SAEM.MI", "color": color_palette[9], "linestyle": "-", "marker":"o"},
+
+     "MICE.Random.500.Y.IMP": {"label": "MICE.Random.500.IMP", "color": color_palette[8], "linestyle": "-", "marker":"o"},
+     "MICE.Cart.10.Y.IMP": {"label": "MICE.Cart.10.Y.IMP", "color": color_palette[8], "linestyle": "-", "marker":"o"},
+     "MICE.Cart.100.Y.IMP": {"label": "MICE.Cart.100.Y.IMP", "color": color_palette[8], "linestyle": "--", "marker":"x"},
+     "MICE.RF.10.Y.IMP": {"label": "MICE.RF.10.Y.IMP", "color": color_palette[8], "linestyle": "-", "marker":"o"},
+     "MICE.Caliber.10.Y.IMP": {"label": "MICE.Caliber.10.Y.IMP", "color": color_palette[8], "linestyle": "-", "marker":"o"},
+     "DRF.100.IMP": {"label": "DRF.100.IMP", "color": color_palette[8], "linestyle": "--", "marker":"x"},
+
+}
+
+methods_no_beta_estimate = [
+    "PbP",
+]
+
+methods_no_pred_estimate = [
+    "CC",
+]
