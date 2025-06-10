93819f3faf40ad519b6613b999e33682d078e7a2
mullerchristophe21@gmail.com <mullerchristophe21@gmail.com>
2025-06-10 14:16:45 +0200

    improve plot files simA

--- plots_scripts/SimA_per_pattern.py
+++ plots_scripts/SimA_per_pattern.py
@@ -1,6 +1,6 @@
 #####
 #
-# Plot main results (4 metrics), Simulation A
+# Plot results per pattern (1 metrics), Simulation A
 #
 #####
 
@@ -28,12 +28,14 @@ score_matrix = score_matrix[score_matrix["exp"] == exp]
 
 metrics_config
 
-# %% 
 
-from utils import calculate_ymin_for_R_proportion
-score_matrix = score_matrix[score_matrix["filter"] == "all"]
+# %%
 
-methods_sel = [
+metric_sel = "mae_bayes"
+patterns_sel = [0, 1, 2, 3, 4]
+patterns_sel = [str(pattern) for pattern in patterns_sel]
+
+sel_methods = [
 "MICE.1.IMP","MICE.1.Y.IMP",
 "MICE.1.M.IMP","MICE.1.Y.M.IMP",
 "MICE.1.IMP.M","MICE.1.Y.IMP.M",
@@ -56,17 +58,17 @@ methods_sel = [
 "MICE.RF.10.IMP.M","MICE.RF.10.Y.IMP.M","MICE.RF.10.M.IMP.M","MICE.RF.10.Y.M.IMP.M"
 ]
 
-methods_sel = [
+sel_methods = [
 "MICE.1.IMP","MICE.1.Y.IMP","MICE.1.M.IMP","MICE.1.Y.M.IMP",
 "MICE.1.IMP.M","MICE.1.Y.IMP.M","MICE.1.M.IMP.M","MICE.1.Y.M.IMP.M",
 ]
 
-methods_sel = [
+sel_methods = [
 "MICE.1.IMP","MICE.1.Y.IMP","MICE.1.M.IMP","MICE.1.Y.M.IMP",
 "Mean.IMP","Mean.IMP.M","05.IMP","05.IMP.M",
 ]
 
-methods_sel = [
+sel_methods = [
 "MICE.1.IMP","MICE.1.Y.IMP",
 "MICE.10.IMP","MICE.10.Y.IMP",
 "MICE.100.IMP","MICE.100.Y.IMP",
@@ -75,43 +77,31 @@ methods_sel = [
 "PbP","CC",
 ]
 
-scores_sel = ["misclassification", "calibration", "mse_error", "mae_bayes"]
-filter_bayes = [True, True, False, False]
+score_matrix_sel = score_matrix[score_matrix["metric"] == metric_sel]
+score_matrix_sel = score_matrix_sel[score_matrix_sel["method"].isin(sel_methods)]
+score_matrix_sel = score_matrix_sel[score_matrix_sel["exp"] == exp]
 
-ntrains = [100, 500, 1000, 5000, 10000, 50000]
-
-# ylimsmax = [0.06, 0.035, 1.5, 0.20] ## Single Imputation
-# ylimsmax = [0.12, 0.1, 1.25, 0.3]  ## Selected Procedures
-ylimsmax = [0.05, 0.05, 0.7, 0.15]  ## MICE Procedures
+score_matrix_sel = score_matrix_sel[score_matrix_sel["filter"].isin(patterns_sel)]
 
 
-ylimsmin = calculate_ymin_for_R_proportion(0.03, ylimsmax)
-ylims = [(ylimsmin[i], ylimsmax[i]) for i in range(len(ylimsmax))] 
+score_matrix_sel = score_matrix_sel.groupby(["filter", "method", "n_train"]).agg({"score": ["mean", "std", "count"]}).reset_index()
+score_matrix_sel.columns = ["filter", "method", "n_train", "mean", "sd", "count"]
+score_matrix_sel["se"] = score_matrix_sel["sd"] / np.sqrt(score_matrix_sel["count"])
 
-fig, axes = plt.subplots(1, len(scores_sel), figsize=(4 * len(scores_sel), 5)) # default is 4 * len, 5
+fig, axes = plt.subplots(1, len(patterns_sel), figsize=(4 * len(patterns_sel), 5))
 
-for i, score in enumerate(scores_sel):
+for i, pattern in enumerate(patterns_sel):
 
-    print(i, score)
+    print(i, pattern)
 
     # filter the score
-    score_matrix_sel = score_matrix[score_matrix["metric"] == score]
-    score_matrix_sel = score_matrix_sel[score_matrix_sel["method"].isin(methods_sel)]
-    score_matrix_sel = score_matrix_sel[score_matrix_sel["exp"] == exp]
-    score_matrix_sel = score_matrix_sel[score_matrix_sel["n_train"].isin(ntrains)]
-    score_matrix_sel = score_matrix_sel[score_matrix_sel["bayes_adj"] == filter_bayes[i]]
-
-    # group by method and n_train
-    score_matrix_sel["score"] = score_matrix_sel["score"].astype(float)
-    score_matrix_sel = score_matrix_sel.groupby(["method", "n_train"]).agg({"score": ["mean", "std", "count"]}).reset_index()
-    score_matrix_sel.columns = ["method", "n_train", "mean", "sd", "count"]
-    score_matrix_sel["se"] = score_matrix_sel["sd"] / np.sqrt(score_matrix_sel["count"])
+    score_matrix_pattern = score_matrix_sel[score_matrix_sel["filter"] == pattern]
 
     # plot the mean and se
-    for method in methods_sel:
+    for method in sel_methods:
         method_config = methods_config[method]
 
-        score_matrix_method = score_matrix_sel[score_matrix_sel["method"] == method]
+        score_matrix_method = score_matrix_pattern[score_matrix_pattern["method"] == method]
         axes[i].plot(score_matrix_method["n_train"], score_matrix_method["mean"], label=method_config["label"], 
                      color=method_config["color"], linestyle=method_config["linestyle"],
                      marker=method_config["marker"], markersize=5)
@@ -121,18 +111,16 @@ for i, score in enumerate(scores_sel):
     
     axes[i].set_xscale("log")
     axes[i].set_xlabel("Number of training samples")
-    axes[i].set_ylabel(metrics_config[score]["label"])
-    axes[i].set_title(metrics_config[score]["label"])   
-    # if i == 0:
-    if i == 1:
+    axes[i].set_ylabel(metrics_config[metric_sel]["label"])
+    axes[i].set_title(f"{pattern} Missing Values")   
+    if i == 0:
         axes[i].legend()
     # axes[i].grid()
-    axes[i].set_ylim(ylims[i])
 
-    # line at 
+    axes[i].set_ylim(-0.01, 0.4)
+    # line at
     axes[i].axhline(0, color="black", linestyle="--", linewidth=0.5)
 
 plt.tight_layout()
-# plt.savefig(os.path.join("plots_scripts", "plots", "SimA_SingleImputation.pdf"))
+# plt.savefig(os.path.join("plots_scripts", "plots", "SimA_SingleImputation_per_pattern.pdf"))
 plt.show()
-    
